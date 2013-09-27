3a2827a721e02d33a98a7802b33b131de8caf2e9
odbol <git@odbol.com>
2013-09-26 23:07:41 -0700

    updated other arduino sketches with new baud rate

--- arduino/BLEShieldSS/BLEShieldSS.ino
+++ arduino/BLEShieldSS/BLEShieldSS.ino
@@ -32,8 +32,8 @@ long interval = 1000;
 void setup()  
 {
   // set the data rate for the SoftwareSerial port
-  bleShield.begin(19200);
-  Serial.begin(19200);
+  bleShield.begin(9600); //was 19200 for firmware v1.0.1
+  Serial.begin(9600); //was 19200 for firmware v1.0.1
   randomSeed(analogRead(0));  
 }
 
